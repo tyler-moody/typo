cfa3fb3b7e1fa59d2e082a209886284a98cc180c
Tyler Moody <tmoody@qumulo.com>
2020-04-02 14:55:29 -0700

    close #3

--- typo.py
+++ typo.py
@@ -7,12 +7,19 @@ import random
 import time
 
 class Application:
+    _colors = dict()
     _config = None
     _files = list()
     def __init__(self, config_filename):
         with open(config_filename) as f:
             self._config = json.loads(f.read())
         self.make_files_list()
+        self._colors["untyped"] = 1
+        curses.init_pair(self._colors["untyped"], curses.COLOR_YELLOW, curses.COLOR_BLACK)
+        self._colors["typed"] = 2
+        curses.init_pair(self._colors["typed"], curses.COLOR_GREEN, curses.COLOR_BLACK)
+        self._colors["mistyped"] = 3
+        curses.init_pair(self._colors["mistyped"], curses.COLOR_RED, curses.COLOR_BLACK)
 
     def make_files_list(self):
         source_root = self._config["source_root"]
@@ -43,33 +50,28 @@ class Application:
 
     def typing_screen(self, window):
         window.clear()
-        untyped = 1
-        curses.init_pair(untyped, curses.COLOR_YELLOW, curses.COLOR_BLACK)
-        typed = 2
-        curses.init_pair(typed, curses.COLOR_GREEN, curses.COLOR_BLACK)
-        mistyped = 3
-        curses.init_pair(mistyped, curses.COLOR_RED, curses.COLOR_BLACK)
         (filename, text, start, end) = self.get_text()
         window.addstr(curses.LINES-1, 0, f"{filename}:{start}:{end}")
         window.move(0,0)
         logging.info(f"num lines is {len(text)}")
-        window.addstr(''.join(text), curses.color_pair(untyped))
+        window.addstr(''.join(text), curses.color_pair(self._colors["untyped"]))
         window.move(0,0)
 
         try:
             current_line = 0
             current_char = 0
+            error_count = 0
             key = None
             while True:
                 logging.info(f"line {current_line} char {current_char}")
                 key = window.getkey()
                 logging.debug(f"key is '{key}'")
                 if key == 'KEY_BACKSPACE' or key == curses.KEY_BACKSPACE:
-                    (y,x) = curses.getsyx()
+                    (y,x) = window.getyx()
                     if x > 0:
                         window.move(y,x-1)
                         current_char -= 1
-                        window.addch(text[current_line][current_char], curses.color_pair(untyped))
+                        window.addch(text[current_line][current_char], curses.color_pair(self._colors["untyped"]))
                         window.move(y,x-1)
                 elif key == "KEY_NPAGE":
                     logging.info("page down")
@@ -79,16 +81,18 @@ class Application:
                     logging.info('tab')
                     while text[current_line][current_char] == ' ':
                         current_char += 1
-                        (y,x) = curses.getsyx()
+                        (y,x) = window.getyx()
                         window.move(y,x+1)
                         window.refresh()
                 else:
                     if current_char >= len(text[current_line]):
-                        colors = mistyped
+                        colors = self._colors["mistyped"]
+                        error_count += 1
                     elif key == text[current_line][current_char]:
-                        colors = typed
+                        colors = self._colors["typed"]
                     else:
-                        colors = mistyped
+                        colors = self._colors["mistyped"]
+                        error_count += 1
                     window.addstr(key, curses.color_pair(colors))
                     current_char += 1
                     if key == '\n': 
@@ -100,9 +104,12 @@ class Application:
                         window.refresh()
                         while text[current_line][current_char] == ' ':
                             current_char += 1
-                            (y,x) = curses.getsyx()
+                            (y,x) = window.getyx()
                             window.move(y,x+1)
                             window.refresh()
+                    (y,x) = window.getyx()
+                    window.addstr(curses.LINES-1, curses.COLS-10, f"errors:{error_count}")
+                    window.move(y,x)
                 window.refresh()
         except KeyboardInterrupt:
             # clean up and exit
